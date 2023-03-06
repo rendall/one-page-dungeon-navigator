94f8b8c0372fd96b36f1ccde12212c8f13289878
Rendall <rendall@gmail.com>
2023-03-06 08:50:32 +0200

    Update tests

--- script/gameLoop.spec.ts
+++ script/gameLoop.spec.ts
@@ -1,5 +1,5 @@
-import { Dungeon } from "./dungeon"
-import { game } from "./gameLoop"
+import { Dungeon, exitDirections } from "./dungeon"
+import { actions, game, GameOutput } from "./gameLoop"
 import { parseDungeon } from "./parseDungeon"
 import testDungeon from "../tests/test_dungeon.json"
 
@@ -17,9 +17,15 @@ const minimalDungeon: Dungeon = {
 
 const parsedDungeon = parseDungeon(testDungeon)
 
-describe("gameLoop game()", () => {
+describe("gameLoop bad game()", () => {
   test("Dungeon with no rooms should error with 'Bad Data'", () => {
     expect(() => game(minimalDungeon)("init")).toThrow("Bad data: room 0 not found")
+  })
+
+  test("Good dungeon with bad input should throw", () => {
+    const input = game(parsedDungeon)
+    input("init")
+    expect(() => input("not-an-action")).toThrow("Unknown action not-an-action")
   })
 })
 
@@ -41,35 +47,75 @@ describe("gameLoop good game()", () => {
   test("First output should welcome properly", () => {
     const input = game(parsedDungeon)
     const firstOutput = {
-      action: "init",
-      description: "You are in a 3m x 1m hallway.  ",
+      message:
+        "Test Dungeon\nFor several centuries the Test Dungeon remained sealed. These days it is badly infested by goats.",
+      room: 0,
       end: false,
-      error: undefined,
+      turn: 1,
+      description: "You are in a 7m x 6m room. Here is a mouth-shaped stone double door with a keyhole to the west. ",
+      action: "init",
       exits: [
         {
-          description: "To the east is a dim passage",
-          door: { dir: { x: 1, y: 0 }, id: 18, type: 0, x: 4, y: 0 },
-          isFacing: true,
-          to: 1,
-          towards: "east",
-          type: 0,
-        },
-        {
-          description: "To the west is a way out of the dungeon",
-          door: { dir: { x: 1, y: 0 }, id: 17, type: 3, x: 0, y: 0 },
+          towards: "south",
           isFacing: false,
           to: "outside",
-          towards: "west",
           type: 3,
+          door: { id: 4, x: 0, y: 0, dir: { x: 0, y: -1 }, type: 3 },
+          description: "To the south is a way out of the dungeon",
+        },
+        {
+          towards: "west",
+          isFacing: true,
+          to: 1,
+          type: 5,
+          door: { id: 5, x: -4, y: -1, dir: { x: -1, y: 0 }, type: 5 },
+          description: "To the west are double doors",
         },
       ],
-      message:
-        "Test Dungeon\nThe Test Dungeon is situated on a distant island. Recently an undead soul-sucking goat has made its lair here.",
-      room: 0,
       statuses: ["visited"],
-      turn: 1,
     }
     const output = input("init")
     expect(output).toEqual(firstOutput)
+  })
+
+  describe.each([...exitDirections, ...actions])("Action %s:", (action) => {
+    let input
+    beforeEach(() => {
+      input = game(parsedDungeon)
+      input("init")
+    })
+
+    test(`${action} of room 0 should return expected output`, () => {
+      const output = input(action)
+      expect(output).toMatchObject({ action: action, turn: 2, statuses: expect.arrayContaining(["visited"]) })
+    })
+  })
+
+  describe("search action", () => {
+    let input
+    beforeEach(() => {
+      input = game(parsedDungeon)
+      input("init")
+    })
+
+    test("search should reveal secret door and allow exit", () => {
+      const eastOutput: GameOutput = input("east")
+      expect(eastOutput.message).toBe("You cannot go that way")
+      expect(eastOutput.exits.length).toBe(2)
+
+      const searchOutput = input("search")
+      expect(searchOutput.message).toBe("You discover a secret door to the east!")
+      expect(searchOutput.exits.length).toBe(3)
+
+      const secretOutput = input("east")
+      expect(secretOutput.message).toBe("You go east")
+    })
+
+    test("second search should give 'searched' status", () => {
+      input("search")
+      const output = input("search")
+      expect(output.message).toBe("You find nothing else of interest.")
+      expect(output.statuses).toContain("searched")
+    })
   })
 })
---
+++ tests/test_dungeon.md
@@ -0,0 +1,10 @@
+# Test Dungeon
+
+_For several centuries the house of Iados remained sealed. These days it is badly infested by goats._
+
+1. A mouth-shaped stone double door with a keyhole to the west.
+2. A scale mail tucked under some debris.
+3. A claw-shaped key hidden in a crack of the wall.
+
+Test Dungeon is based on "House of Iados"
+[https://watabou.github.io/one-page-dungeon/?seed=1859006343&tags=small,compact,secret](https://watabou.github.io/one-page-dungeon/?seed=1859006343&tags=small,compact,secret)
